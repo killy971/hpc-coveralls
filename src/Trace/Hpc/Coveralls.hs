82733992dc3ede8895019770c15273d56044f15a
killy971 <killy971@gmail.com>
2014-03-11 23:33:50 +0900

    Added logic to iterate through each TixModule

--- src/Trace/Hpc/Coveralls.hs
+++ src/Trace/Hpc/Coveralls.hs
@@ -3,15 +3,35 @@
 module Trace.Hpc.Coveralls where
 
 import Data.Aeson
-import Data.Text
+import Data.Aeson.Types (emptyArray)
+import Data.String.Utils (replace)
+import Data.Text (pack)
 import Trace.Hpc.Tix
 import qualified Data.ByteString.Lazy.Char8 as BSL
 
 tixDir :: String
 tixDir = "dist/hpc/tix/"
 
+-- is there a better way?
+-- (this is not correct as some files are under src/ and others under test/)
+toFilename :: String -> String
+toFilename = (++ ".hs") . replace "." "/"
+
+-- tixs accessor: tixModuleTicks
+tixModuleToJson :: TixModule -> Value
+tixModuleToJson tix = object [
+    "name" .= filename,
+    "source" .= pack "", -- empty for now
+    "coverage" .= emptyArray] -- empty for now
+    where moduleName = tixModuleName tix
+          filename = pack $ toFilename moduleName
+
 tixToJson :: Tix -> Value
-tixToJson _ = object [ "coveralls-support" .= ("TODO" :: Text)]
+tixToJson (Tix tixModules) = object [
+    "service_job_id" .= pack "0", -- dummy id for now
+    "service_name" .= pack "travis-ci", -- fixed for now
+    "source_files" .= resultList]
+    where resultList = map tixModuleToJson tixModules
 
 generateCoverallsFromTix :: String -> IO ()
 generateCoverallsFromTix name = do
