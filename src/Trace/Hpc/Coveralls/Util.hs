16315af96901dc2e7d58268072fd6510fbc10428
Guillaume Nargeot <killy971+github@gmail.com>
2014-12-17 23:38:34 +0900

    Include all MixEntry of same HpcPos in CoverageEntry

--- src/Trace/Hpc/Coveralls/Util.hs
+++ src/Trace/Hpc/Coveralls/Util.hs
@@ -11,6 +11,15 @@ module Trace.Hpc.Coveralls.Util where
 
 import Data.List
 
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+trd3 :: (a, b, c) -> c
+trd3 (_, _, x) = x
+
 fst4 :: (a, b, c, d) -> a
 fst4 (x, _, _, _) = x
 
