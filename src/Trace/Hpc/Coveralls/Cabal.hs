3460a641e9a8807a054c82eef9d83a089107a16f
Guillaume Nargeot <killy971+github@gmail.com>
2015-03-25 23:15:49 +0900

    Added functions to extract package version from cabal configuration file

---
+++ src/Trace/Hpc/Coveralls/Cabal.hs
@@ -0,0 +1,39 @@
+-- |
+-- Module:      Trace.Hpc.Coveralls.Cabal
+-- Copyright:   (c) 2014-2015 Guillaume Nargeot
+-- License:     BSD3
+-- Maintainer:  Guillaume Nargeot <guillaume+hackage@nargeot.com>
+-- Stability:   experimental
+--
+-- Functions for reading the cabal package name and version.
+
+module Trace.Hpc.Coveralls.Cabal where
+
+import Control.Applicative
+import Control.Monad
+import Data.List (isSuffixOf)
+import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Configuration
+import Distribution.PackageDescription.Parse
+import Distribution.Version
+import System.Directory
+
+getCabalFile :: IO (Maybe FilePath)
+getCabalFile = do
+    cnts <- (filter isCabal <$> getDirectoryContents ".") >>= filterM doesFileExist
+    case cnts of
+        [file] -> return $ Just file
+        _ -> return Nothing
+    where isCabal filename = ".cabal" `isSuffixOf` filename && length filename > 6
+
+getPackageNameVersion :: FilePath -> IO (Maybe String)
+getPackageNameVersion file = do
+    orig <- readFile "lens.cabal"
+    case parsePackageDescription orig of
+        ParseFailed _ -> return Nothing
+        ParseOk _warnings gpd -> do
+            let pkg = package . packageDescription $ gpd
+                PackageName name = pkgName pkg
+                version = pkgVersion pkg
+            return $ Just $ name ++ "-" ++ (show version)
