344217f513b7adfb9037f73026f5d928be98d07f
Ryan Scott <ryan.gl.scott@gmail.com>
2018-03-09 13:33:44 -0500

    Allow building with Cabal-2.2

--- src/Trace/Hpc/Coveralls/Cabal.hs
+++ src/Trace/Hpc/Coveralls/Cabal.hs
@@ -15,9 +15,14 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans.Maybe
 import Data.List (intercalate, isSuffixOf)
+import Data.String
 import Distribution.Package
 import Distribution.PackageDescription
+#if MIN_VERSION_Cabal(2,2,0)
+import Distribution.PackageDescription.Parsec
+#else
 import Distribution.PackageDescription.Parse
+#endif
 import Distribution.Version
 import System.Directory
 
@@ -32,13 +37,22 @@ getCabalFile dir = do
 getPackageNameVersion :: FilePath -> IO (Maybe String)
 getPackageNameVersion file = do
     orig <- readFile file
-    case parsePackageDescription orig of
-        ParseFailed _ -> return Nothing
-        ParseOk _warnings gpd -> return $ Just $ name ++ "-" ++ version
+    case runParseResult (parseGenericPackageDescription (fromString orig)) of
+        (_warnings, Left _) -> return Nothing
+        (_warnings, Right gpd) -> return $ Just $ name ++ "-" ++ version
             where pkg = package . packageDescription $ gpd
                   name = unPackageName $ pkgName pkg
                   version = showVersion (pkgVersion pkg)
                   showVersion = intercalate "." . map show . versionNumbers
+  where
+#if !(MIN_VERSION_Cabal(2,0,0))
+    parseGenericPackageDescription = parsePackageDescription
+#endif
+
+#if !(MIN_VERSION_Cabal(2,2,0))
+    runParseResult (ParseFailed err)      = ([],       Left (Nothing, [err]))
+    runParseResult (ParseOk warnings gpd) = (warnings, Right gpd)
+#endif
 
 currDirPkgNameVer :: IO (Maybe String)
 currDirPkgNameVer = runMaybeT $ pkgNameVersion currentDir
