a2d500316fecb8ee49c034e2781862c8606b96af
Ben Gamari <ben@smart-cactus.org>
2018-12-24 14:43:49 -0500

    Add support for Cabal-2.4

--- src/Trace/Hpc/Coveralls/Cabal.hs
+++ src/Trace/Hpc/Coveralls/Cabal.hs
@@ -17,7 +17,12 @@ import Control.Monad.Trans.Maybe
 import Data.List (intercalate, isSuffixOf)
 import Distribution.Package
 import Distribution.PackageDescription
+#if MIN_VERSION_Cabal(2,4,0)
+import Distribution.PackageDescription.Parsec
+import qualified Data.ByteString as BS
+#else
 import Distribution.PackageDescription.Parse
+#endif
 import Distribution.Version
 import System.Directory
 
@@ -30,6 +35,17 @@ getCabalFile dir = do
     where isCabal filename = ".cabal" `isSuffixOf` filename && length filename > 6
 
 getPackageNameVersion :: FilePath -> IO (Maybe String)
+#if MIN_VERSION_Cabal(2,4,0)
+getPackageNameVersion file = do
+    orig <- BS.readFile file
+    case runParseResult $ parseGenericPackageDescription orig of
+        (_warnings, Left _) -> return Nothing
+        (_warnings, Right gpd) -> return $ Just $ name ++ "-" ++ version
+            where pkg = package . packageDescription $ gpd
+                  name = unPackageName $ pkgName pkg
+                  version = showVersion (pkgVersion pkg)
+                  showVersion = intercalate "." . map show . versionNumbers
+#else
 getPackageNameVersion file = do
     orig <- readFile file
     case parsePackageDescription orig of
@@ -39,6 +55,7 @@ getPackageNameVersion file = do
                   name = unPackageName $ pkgName pkg
                   version = showVersion (pkgVersion pkg)
                   showVersion = intercalate "." . map show . versionNumbers
+#endif
 
 currDirPkgNameVer :: IO (Maybe String)
 currDirPkgNameVer = runMaybeT $ pkgNameVersion currentDir
